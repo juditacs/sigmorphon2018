86925b8e19272ddf3336c2f8caedf941f9694c11
Judit Acs <judit@sch.bme.hu>
2018-08-28 23:25:48 +0200

    remove batch size 16

--- scripts/evaluate_dirs.sh
+++ scripts/evaluate_dirs.sh
@@ -20,7 +20,7 @@ for exp_dir in "$@"; do
         echo "----------------------"
         echo "EXPERIMENT $exp_dir"
         echo "   Evaluating train file: $(basename $train_file)"
-        python $INFERENCE_SRC -e $exp_dir -t $train_file --override-param batch_size=16 > $exp_dir/train.out
+        python $INFERENCE_SRC -e $exp_dir -t $train_file > $exp_dir/train.out
         if [ $? -eq 0 ]; then
             paste $train_file <( cut -f2 $exp_dir/train.out ) | sed 's/ //g' | awk 'BEGIN{FS="\t"}{if($2==$NF)c++;s++}END{print c/s}' > $exp_dir/train.word_accuracy
             echo "   " $(cat $exp_dir/train.word_accuracy)
@@ -31,12 +31,12 @@ for exp_dir in "$@"; do
 
     dev_file=$(grep ^dev_file $exp_dir/config.yaml)
     dev_file=${dev_file#dev_file: }
-    if [ ! -f $exp_dir/dev.batch16.word_accuracy ] || [ $exp_dir/model -nt $exp_dir/dev.batch16.word_accuracy ]; then
+    if [ ! -f $exp_dir/dev.word_accuracy ] || [ $exp_dir/model -nt $exp_dir/dev.word_accuracy ]; then
         echo "   Evaluating dev file: $(basename $dev_file)"
-        python $INFERENCE_SRC -e $exp_dir -t $dev_file --override-param batch_size=16 > $exp_dir/dev.batch16.out
+        python $INFERENCE_SRC -e $exp_dir -t $dev_file > $exp_dir/dev.out
         if [ $? -eq 0 ]; then
-            paste $dev_file <( cut -f2 $exp_dir/dev.batch16.out ) | sed 's/ //g' | awk 'BEGIN{FS="\t"}{if($2==$NF)c++;s++}END{print c/s}' > $exp_dir/dev.batch16.word_accuracy
-            echo "   " $(cat $exp_dir/dev.batch16.word_accuracy)
+            paste $dev_file <( cut -f2 $exp_dir/dev.out ) | sed 's/ //g' | awk 'BEGIN{FS="\t"}{if($2==$NF)c++;s++}END{print c/s}' > $exp_dir/dev.word_accuracy
+            echo "   " $(cat $exp_dir/dev.word_accuracy)
         else
             echo "   Inference failed"
         fi
